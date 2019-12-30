6d32528462deeff932001564b6c765bf45d53b02
bhgottfried <35643275+bhgottfried@users.noreply.github.com>
2019-12-29 21:47:49 -0500

    Airplane create octree from binary file
    Airplane flight 1 load

---
+++ C_single/octree.h
@@ -0,0 +1,35 @@
+#ifndef __OCTREE_H_
+#define __OCTREE_H_
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+#define NUM_FIELDS 3
+#define FIELD_SIZE (sizeof(float))
+
+
+typedef struct _Point
+{
+	float coords[NUM_FIELDS];
+} Point;
+
+typedef struct _OctreeNode
+{
+	Point* data;
+	struct _OctreeNode* children[8];
+} OctreeNode;
+
+
+// Point functions
+unsigned int parse_header(FILE* const fp);
+Point** read_points(FILE* const fp, unsigned int numPoints);
+void get_min_max(Point** points, unsigned int numPoints, float* fieldMins, float* fieldMaxs);
+// Octree functions
+OctreeNode* create_octree(Point** points, unsigned int numPoints, float* fieldMins, float* fieldMaxs);
+void delete_octree(OctreeNode* root);
+
+
+#endif
--- C_single/single.c
+++ C_single/single.c
@@ -1,27 +1,58 @@
-#include "single.h"
+#include "octree.h"
 
-OctreeNode* init_octree(char data)
+
+// Call syntax: ./single [relative path to .pcd binary file to compress]
+int main(int argc, char* argv[])
 {
-	OctreeNode* node = malloc(sizeof(*node));
-	node->data = data;
-	for (int i = 0; i < 8; i++)
+	if (argc != 2)
 	{
-		node->children[i] = NULL;
+		printf("Incorrect number of arguments.\n");
+		return EXIT_FAILURE;
 	}
-	return node;
-}
 
-void delete_octree(OctreeNode* root)
-{
-	if (root)
+	FILE* fp = fopen(argv[2], "rb");
+	if (!fp)
 	{
-		for (int i = 0; i < 8; i++)
-		{
-			delete_octree(root->children[i]);
-		}
-		free(root);
+		printf("Failed to open file.\n");
+		return EXIT_FAILURE;
 	}
+
+	// Read number of points from header
+	unsigned int numPoints = parse_header(fp);
+	if (!numPoints)
+	{
+		printf("Failed to parse .pcd header.\n");
+		fclose(fp);
+		return EXIT_FAILURE;
+	}
+	
+	// Create array of 3D points
+	Point** points = read_points(fp, numPoints);
+	if (!points)
+	{
+		printf("Failed to parse point data.\n");
+		fclose(fp);
+		return EXIT_FAILURE;
+	}
+
+	// Find min and max values for each field
+	float fieldMins[NUM_FIELDS];
+	float fieldMaxs[NUM_FIELDS];
+	get_min_max(points, numPoints, fieldMins, fieldMaxs);
+
+	// Create octree for the read points
+	OctreeNode* root = create_octree(points, numPoints, fieldMins, fieldMaxs);
+
+	// TODO compress
+
+	// Clean up dynamicaly allocated memory and files
+	delete_octree(root);
+	for (unsigned int ptIdx = 0; ptIdx < numPoints; ptIdx++)
+	{
+		free(points[ptIdx]);
+	}
+	free(points);
+	fclose(fp);
+
+	return EXIT_SUCCESS;
 }
-
-
-
