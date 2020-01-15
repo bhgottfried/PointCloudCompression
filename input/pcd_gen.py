8d50b190cca8f901d8686bbb2fe39594e68b6f34
bhgottfried <bhgottfried@gmail.com>
2020-01-15 11:59:27 -0500

    Fixed spacing issue that caused invalid parsing

--- input/pcd_gen.py
+++ input/pcd_gen.py
@@ -9,12 +9,13 @@ if len(sys.argv) < 2:
 numPoints = int(sys.argv[1])
 
 with open("rand{0}.pcd".format(numPoints), "wb") as outFile:
-	outFile.write(("VERSION .7\n\
-					FIELDS x y z\n\
-					SIZE 4 4 4\n\
-					TYPE f f f\n\
-					COUNT 1 1 1\n\
-					WIDTH {0}\n".format(numPoints)
-				+  "HEIGHT 1\nVIEWPOINT 0 0 0 1 0 0 0\n\
-					POINTS {0}\nDATA binary\n".format(numPoints)).encode("ascii"))
+	outFile.write(("VERSION .7\n"
+				+ "FIELDS x y z\n"
+				+ "SIZE 4 4 4\n"
+				+ "TYPE f f f\n"
+				+ "COUNT 1 1 1\n"
+				+ "WIDTH {0}\n".format(numPoints)
+				+ "HEIGHT 1\nVIEWPOINT 0 0 0 1 0 0 0\n"
+				+ "POINTS {0}\n".format(numPoints)
+				+ "DATA binary\n").encode("ascii"))
 	array("f", [random() * 2 - 1 for _ in range(numPoints * 3)]).tofile(outFile)
