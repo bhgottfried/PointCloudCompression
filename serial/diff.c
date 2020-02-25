e87f94af2a3901dc7e9da38e78b51fb9376f750e
bhgottfried <bhgottfried@gmail.com>
2020-02-24 23:09:21 -0500

    Merge implementation

--- serial/diff.c
+++ serial/diff.c
@@ -172,6 +172,85 @@ OctreeNode* reconstruct_from_diff(const OctreeNode* const prevTree, const ByteLi
 // Merge two diff lists to get the difference from Ti to Tk
 ByteList* merge_diff(const ByteList* const Dij, const ByteList* const Djk)
 {
+	if (!Dij || ! Djk)
+	{
+		printf("Invalid arguments to merge_diff.\n");
+		return NULL;
+	}
 
-	return NULL;
+	const unsigned char zero = 0;	// This will make sense later...
+
+	// Initialize resulting, merged list
+	ByteList* sum = malloc(sizeof(*sum));
+	sum->head = malloc(sizeof(*sum->head));
+	sum->numBytes = 0;
+
+	// Current links for all linked lists
+	Link* mergedLink = sum->head;
+	Link* currOld = Dij->head;
+	Link* currNew = Djk->head;
+	
+	// Initialize queues to hold data bytes for each diff list
+	Queue* Qold = init_queue();
+	Queue* Qnew = init_queue();
+	enqueue(Qold, &currOld->data);
+	enqueue(Qnew, &currNew->data);
+	currOld = currOld->next;
+	currNew = currNew->next;
+
+	while (!is_empty(Qold) || !is_empty(Qnew))
+	{
+		// Get next diff data bytes that correspond to the same subvoxel and OR/XOR them
+		unsigned char oldByte = *(unsigned char*) dequeue(Qold);
+		unsigned char newByte = *(unsigned char*) dequeue(Qnew);
+		unsigned char xByte = oldByte ^ newByte;
+		unsigned char oByte = oldByte | newByte;
+		
+		// Append the merged difference byte to the linked list
+		if (sum->numBytes++)
+		{
+			mergedLink->next = malloc(sizeof(*mergedLink));
+			mergedLink = mergedLink->next;
+		}
+		mergedLink->data = xByte;
+		mergedLink->next = NULL;
+		sum->tail = mergedLink;
+
+		// Interleave diff bytes into queues. If they do not exist, enqueue a zero byte
+		for (int childIdx = 0; childIdx < 8; childIdx++)
+		{
+			if (oByte & (1 << childIdx))
+			{
+				if (oldByte & (1 << childIdx))
+				{
+					enqueue(Qold, &currOld->data);
+					if (currOld)
+					{
+						currOld = currOld->next;
+					}
+				}
+				else
+				{
+					enqueue(Qold, &zero);
+				}
+
+				if (newByte & (1 << childIdx))
+				{
+					enqueue(Qnew, &currNew->data);
+					if (currNew)
+					{
+						currNew = currNew->next;
+					}
+				}
+				else
+				{
+					enqueue(Qnew, &zero);
+				}
+			}
+		}
+	}
+	
+	delete_queue(Qold);
+	delete_queue(Qnew);
+	return sum;
 }
