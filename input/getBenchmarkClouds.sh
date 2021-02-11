95fddc270e5f34d1ad794be07501b05bf9732e92
Ben Gottfried <bhgottfried@gmail.com>
2021-02-11 14:17:44 -0500

    Fix cloud creation script

--- input/getBenchmarkClouds.sh
+++ input/getBenchmarkClouds.sh
@@ -2,8 +2,7 @@
 
 python3 pcd_gen.py 1000 "./clouds/test0000.pcd"
 prev="0000"
-for i in $(seq -f "%04g" 1 8096); \
-	do \
-	python3 pcd_shift.py 50 "./clouds/test${prev}.pcd" "./clouds/test${i}.pcd" \
-	prev=i
-	done
+for i in $(seq -f "%04g" 1 1025); do
+	python3 pcd_shift.py 50 "./clouds/test${prev}.pcd" "./clouds/test${i}.pcd"
+	prev=$i
+done
